b22f3a1d2653e83b689ca703e54e73a9e1d5ab42
Sebastian <sebastian@hirts.de>
2021-07-21 03:01:01 +0200

    deleting everything

--- complex_numbers.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt, mpld3
-
-fig = plt.figure()
-plt.plot([2,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
-html = mpld3.fig_to_html(fig)
-
-mpld3.save_html(fig, "complex_plane.html")
-
